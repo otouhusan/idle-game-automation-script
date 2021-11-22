f8672b7028aa673e82bb4ca7ae9a158ed55d3ae8
otouhusan <34184346+otouhusan@users.noreply.github.com>
2021-11-23 05:38:45 +0900

    Update holyCalcEvo.js

--- holyCalcEvo.js
+++ holyCalcEvo.js
@@ -3,13 +3,13 @@ javascript:(
       
       console.log('ページタイトル');
       alert('ページタイトル：');
-//       var newElement = document.createElement("span");
-//       var newContent = document.createTextNode("holy");
-//       newElement.appendChild(newContent);
-//       newElement.setAttribute("class","tabNav t1Nav");
-//       var parentDiv = document.getElementById("mainNav");
-//       var result = parentDiv.appendChild(newElement);
-//       console.log(result);
+      var newElement = document.createElement("span");
+      var newContent = document.createTextNode("holy");
+      newElement.appendChild(newContent);
+      newElement.setAttribute("class","tabNav t1Nav");
+      var parentDiv = document.getElementById("mainNav");
+      var result = parentDiv.appendChild(newElement);
+      console.log(result);
     }
 
   )();
